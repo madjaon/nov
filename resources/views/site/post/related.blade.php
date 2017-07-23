1596569d7fb4f9cea2583194d96bd6ab463bb39b
Mad Raon <muadauyeu24@gmail.com>
2017-07-23 18:39:50 +0700

    fix

--- resources/views/site/post/related.blade.php
+++
@@ -1,9 +0,0 @@
-@if($data && count($postData) > 0)
-<div class="related">
-	<div class="box-title clearfix">
-		<h3><a href="{!! $typeMainUrl !!}" title="{!! $data->name !!}">{!! $data->name !!}</a></h3>
-		<span>&nbsp;</span>
-	</div>
-	@include('site.post.box0', array('data' => $postData))
-</div>
-@endif
