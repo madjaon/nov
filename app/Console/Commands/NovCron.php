054357e975da63e000458db8a19d0487039956a5
Mad Raon <muadauyeu24@gmail.com>
2017-08-03 03:19:32 +0700

    fix

--- app/Console/Commands/NovCron.php
+++ app/Console/Commands/NovCron.php
@@ -3,40 +3,52 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use DB;
 
 class NovCron extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'nov:cron';
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'nov:cron';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'Command description';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
+  /**
+   * Create a new command instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    parent::__construct();
+  }
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
-        $this->info('Nov:Cron Cummand Run successfully!');
-    }
+  /**
+   * Execute the console command.
+   *
+   * @return mixed
+   */
+  public function handle()
+  {
+    $this->info('Nov:Cron Cummand Run successfully!');
+  }
+
+  private function getPostContinue()
+  {
+    $data = DB::table('posts')
+              ->select('id', 'source', 'source_url')
+              ->where('kind', SLUG_POST_KIND_UPDATING)
+              ->where('status', ACTIVE)
+              ->where('start_date', '<=', date('Y-m-d H:i:s'))
+              ->get();
+    return $data;
+  }
 }
