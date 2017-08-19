2f031135129a624f415e2b7a990e6c0c08a855a2
Mad Raon <muadauyeu24@gmail.com>
2017-08-19 23:53:42 +0700

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         // Commands\Inspire::class,
-        Commands\NovCron::class,
+        // Commands\NovCron::class,
     ];
 
     /**
